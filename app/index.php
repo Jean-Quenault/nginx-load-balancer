73ab02709c1b3a6edae86aa9a69d767c8d1e480e
Jean-Quenault <jeanform@proton.me>
2023-12-22 17:04:57 +0100

    First commit

---
+++ app/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Server Hostname</title>
+</head>
+<body>
+    <h1>Hostname of this Server:</h1>
+    <p><?php echo gethostname(); ?></p>
+</body>
+</html>
